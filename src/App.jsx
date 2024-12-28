8948630c520a9d468c293d0f88f534a72a9de9c5
khaleedcode <164218329+khaleedcode@users.noreply.github.com>
2024-12-28 19:51:46 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,8 @@ function App() {
   return (
       <div className='canvas'>
         <GlobalStyles />
-        <div>haha</div>
         <UpperMenu />
         <TypingBox />
-        <div>hehe</div>
       </div>
   )
 }
